58ffbd3aba66d34876be0b817a395ecd533443f8
Riccardo Petracchini <riccardopetracchini@gmail.com>
2018-06-22 14:36:52 +0200

    better folderpath check

--- bin/JsonDataExtractor.js
+++ bin/JsonDataExtractor.js
@@ -8,12 +8,24 @@ const getConfig = require('../lib/getConfig'),
 const config = getConfig();
 if (!config) return;
 
+if (config.JSON && config.JSON.folderpath)
+    if (((typeof config.JSON.folderpath != "undefined") &&
+            (typeof config.JSON.folderpath.valueOf() == "string")) &&
+        (config.JSON.folderpath.length > 0)) {
+        // folder configurato
+        const folder = config.JSON.folderpath;
 
-const folder = config.JSON.folderpath;
+        if (helper.isDirectory(folder));
 
-console.log(colors.success(`\n Found a JSON ${folder}  configured !`));
+        console.log(colors.success(`\n Found a JSON ${folder}  configured !`));
 
-var JSONfiles = helper.getJSONFiles(folder);
+        var JSONfiles = helper.getJSONFiles(folder);
 
-// Go
-jsonExtracted.analyze(JSONfiles);
+        // Go
+        jsonExtracted.analyze(JSONfiles);
+    } else {
+        //fs.mkdirSync(directory);
+        console.log(colors.error(`\n config.json > checking folder path: ${folder}`));
+        console.log(colors.error(`\nATTENTION! Folder path is not valid or is not configurated in config.json!`));
+    }
+}
